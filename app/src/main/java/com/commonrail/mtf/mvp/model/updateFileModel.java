4b306cfcc501f60483a9baa07100f4446170acc0
emin <@youremail.com>
2016-03-01 23:16:20 +0800

    user mvp of mainactivty

---
+++ app/src/main/java/com/commonrail/mtf/mvp/model/updateFileModel.java
@@ -0,0 +1,19 @@
+package com.commonrail.mtf.mvp.model;
+
+import com.commonrail.mtf.mvp.presenter.OnUpdateFileListener;
+import com.commonrail.mtf.util.Api.RtApi;
+
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * 项目名称：railtool
+ * 类描述：
+ * 创建人：wengyiming
+ * 创建时间：16/3/1 下午9:50
+ * 修改人：wengyiming
+ * 修改时间：16/3/1 下午9:50
+ * 修改备注：
+ */
+public interface UpdateFileModel {
+    void checkFileUpdate(CompositeSubscription subscription,RtApi api, OnUpdateFileListener listener);
+}
---
+++ app/src/main/java/com/commonrail/mtf/mvp/presenter/OnupdateFileListener.java
@@ -0,0 +1,26 @@
+package com.commonrail.mtf.mvp.presenter;
+
+import com.commonrail.mtf.mvp.model.entity.FileUpload;
+
+/**
+ * 项目名称：railtool
+ * 类描述：
+ * 创建人：wengyiming
+ * 创建时间：16/3/1 下午9:50
+ * 修改人：wengyiming
+ * 修改时间：16/3/1 下午9:50
+ * 修改备注：
+ */
+public interface OnUpdateFileListener {
+    /**
+     * 成功时回调
+     *
+     * @param t t
+     */
+    void onUpdateFilesSuccess(FileUpload t);
+
+    /**
+     * 失败时回调，简单处理，没做什么
+     */
+    void onUpdateFilesError();
+}
