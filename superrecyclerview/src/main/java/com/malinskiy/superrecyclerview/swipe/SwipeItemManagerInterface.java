1f369208662258f2a0911d0be7ade3fd651e2641
emin <@youremail.com>
2016-03-27 23:03:44 +0800

    superrecyclerview
    superrecyclerview

---
+++ superrecyclerview/src/main/java/com/malinskiy/superrecyclerview/swipe/SwipeItemManagerInterface.java
@@ -0,0 +1,29 @@
+package com.malinskiy.superrecyclerview.swipe;
+
+import java.util.List;
+
+public interface SwipeItemManagerInterface {
+
+    void openItem(int position);
+
+    void closeItem(int position);
+
+    void closeAllExcept(SwipeLayout layout);
+
+    List<Integer> getOpenItems();
+
+    List<SwipeLayout> getOpenLayouts();
+
+    void removeShownLayouts(SwipeLayout layout);
+
+    boolean isOpen(int position);
+
+    Mode getMode();
+
+    void setMode(Mode mode);
+
+
+    enum Mode {
+        Single, Multiple
+    }
+}
